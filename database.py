17cad6f3b362d5a0220e3d361c7e2504e952ca60
Максим Альфа <maksabasov1998@gmail.com>
2025-01-30 08:55:12 +0300

    database

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQL_DB_URL = 'sqlite:///./itproger.db'
+
+engine = create_engine(SQL_DB_URL)
+session_local = sessionmaker(autoflush=False, autocommit = False, bind = engine)
+
+Base = declarative_base()
